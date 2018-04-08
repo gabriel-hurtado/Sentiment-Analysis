c0f9739ff019408b85824f20b0ac0385f2a1eef8
Melan <melan2308@gmail.com>
2018-04-08 17:27:32 +0200

    Set random_state to seed variable for train/text split

--- deep_learning/ConvNet.py
+++ deep_learning/ConvNet.py
@@ -83,7 +83,7 @@ def count_vectorize(data):
 X = toy_data["text"].tolist()
 y = toy_data["stars"].tolist()
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=seed)
 
 X_train_counts, count_vectorizer = count_vectorize(X_train)
 X_test_counts = count_vectorizer.transform(X_test)
