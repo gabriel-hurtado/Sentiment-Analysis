f1db1dc5f49fa1410dadf12acfdfea5c7f5cc2b7
Melan <melan2308@gmail.com>
2018-04-08 17:10:45 +0200

    Complete script for ConvNet model that save model and its weights

--- deep_learning/ConvNet.py
+++ deep_learning/ConvNet.py
@@ -3,18 +3,20 @@ Script that train a ConvNet model for the Yelp dataset
 This is inspired from Yoon Kim paper : 
 https://arxiv.org/abs/1408.5882
 
+# Note #1: 
 To execute the script, you need to download the 
 Google's pre-trained Word2Vec model available at : 
 https://code.google.com/archive/p/word2vec/ 
+
+# Note #2: 
+You might need to install gensim package using either: 
+- pip install gensim 
+- conda install -c anaconda gensim (anaconda environment)
 
 # Compile command : 
 	python ConvNet.py --dataset ../yelp_academic_dataset_review.pickle 
 	--word2vec ../GoogleNews-vectors-negative300.bin
 
-# Note : 
-You might need to install gensim package : 
-pip install gensim 
-conda install -c anaconda gensim (anaconda environment)
 """
 
 ## Import libraries
@@ -52,5 +54,178 @@ parser.add_argument("-w", "--word2vec", require=True, \
 	help='path to word2vec pre-trained model')
 args = parser.parse_args
 
+# Retrieve the path to files
 path_data = args.dataset
 path_word2vec = args.word2vec
+
+
+## Load data
+data = pd.read_pickle(path_data)
+
+# Removing all ('\n') characters using list comprehensions
+data['text'] = [txt.replace('\n', '') for txt in data['text']]
+
+# Taking only text and stars columns
+data = data.loc[:, ['text', 'stars']]
+
+
+## Tokenize sentences 
+data["tokens"] = data.apply(lambda row: word_tokenize(row["text"]), axis=1)
+
+
+## Bag of words counts
+def count_vectorize(data):
+    count_vectorizer = CountVectorizer()
+    
+    embedding = count_vectorizer.fit_transform(data)
+    
+    return embedding, count_vectorizer
+
+X = toy_data["text"].tolist()
+y = toy_data["stars"].tolist()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+X_train_counts, count_vectorizer = count_vectorize(X_train)
+X_test_counts = count_vectorizer.transform(X_test)
+
+
+## Load word2vec model 
+word2vec = gensim.models.KeyedVectors.load_word2vec_format(path_word2vec, binary=True)
+
+
+## Embed the text
+all_words = [word for tokens in data["tokens"] for word in tokens]
+sentence_lengths = [len(tokens) for tokens in data["tokens"]]
+vocabulary = sorted(list(set(all_words)))
+
+EMBEDDING_DIM = 300
+MAX_SEQUENCE_LENGTH = 35
+VOCAB_SIZE = len(vocabulary)
+VALIDATION_SPLIT = 0.2
+
+tokenizer = Tokenizer(num_words=VOCAB_SIZE)
+tokenizer.fit_on_texts(data["text"].tolist())
+sequences = tokenizer.texts_to_sequences(data["text"].tolist())
+
+word_index = tokenizer.word_index
+print("Found %s unique tokens." % (len(word_index)))
+
+cnn_data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+labels = to_categorical(np.asarray(data["stars"]))
+
+indices = np.arange(cnn_data.shape[0])
+np.random.shuffle(indices)
+cnn_data = cnn_data[indices]
+labels = labels[indices]
+num_validation_samples = int(VALIDATION_SPLIT * cnn_data.shape[0])
+
+embedding_weights = np.zeros((len(word_index) + 1, EMBEDDING_DIM))
+
+for word, index in word_index.items():
+    embedding_weights[index, :] = word2vec[word] if word in word2vec else np.random.rand(EMBEDDING_DIM)
+
+print("embedding_weights shape: ", embedding_weights.shape)
+
+## Construct model 
+def ConvNet(embeddings, max_sequence_length, num_words, embedding_dim, trainable=False):
+    
+    embedding_layer = Embedding(num_words, \
+                                embedding_dim, \
+                                weights=[embeddings], \
+                                input_length=max_sequence_length, \
+                                trainable=trainable)
+    
+    sequence_input = Input(shape=(max_sequence_length, ), dtype="int32", name="Input")
+    embedded_sequences = embedding_layer(sequence_input)
+    
+    convs = []
+    filter_sizes = [3, 4, 5]
+    
+    for filter_size in filter_sizes:
+        l_conv = Conv1D(filters=128, kernel_size=filter_size, activation='relu')(embedded_sequences)
+        l_pool = MaxPooling1D(pool_size=3)(l_conv)
+        convs.append(l_pool)
+    
+    l_merge = Merge(mode='concat', concat_axis=1, name="Merge")(convs)
+    
+    l_conv1 = Conv1D(filters=128, kernel_size=5, activation='relu')(l_merge)
+    l_pool1 = MaxPooling1D(pool_size=3)(l_conv1)
+    
+    l_drop  = Dropout(0.5)(l_pool1)
+    
+    l_flat  = Flatten(name="Flatten")(l_drop)
+    
+    l_dense = Dense(128, activation='relu', name="Dense")(l_flat)
+    
+    
+    preds = Dense(6, activation='softmax', name="Output")(l_dense)
+    
+    model = Model(sequence_input, preds)
+    
+    return model
+
+
+X_train = cnn_data[:-num_validation_samples]
+y_train = labels[:-num_validation_samples]
+X_val  = cnn_data[-num_validation_samples:]
+y_val  = labels[-num_validation_samples:]
+
+model = ConvNet(embedding_weights, \
+                MAX_SEQUENCE_LENGTH, \
+                len(word_index)+1, \
+                EMBEDDING_DIM, \
+                trainable=False)
+
+
+## Training the model
+model.compile(loss='categorical_crossentropy', \
+              optimizer='adam', \
+              metrics=['acc'])
+
+history = model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=5, batch_size=128)
+
+
+## Serialize model to JSON
+model_json = model.to_json()
+with open("model_convnet.json", "w") as json_file:
+    json_file.write(model_json)
+    
+## Serialize weights to HDF5
+
+model.save_weights("model_convnet.h5")
+print("Saved model to disk")
+
+
+## Plotting the training and validation loss 
+history_dict = history.history
+train_loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+
+num_epochs = 5
+epochs = range(1, num_epochs + 1)
+
+plt.plot(epochs, train_loss_values, 'r', label="Training loss")
+plt.plot(epochs, val_loss_values, 'b', label="Validation loss")
+plt.title("Training and validation loss")
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.legend()
+
+## Plotting the training and validation accuracy 
+history_dict = history.history
+train_acc_values = history_dict['acc']
+val_acc_values = history_dict['val_acc']
+
+num_epochs = 5
+epochs = range(1, num_epochs + 1)
+
+plt.plot(epochs, train_acc_values, 'r', label="Training accuracy")
+plt.plot(epochs, val_acc_values, 'b', label="Validation accuracy")
+plt.title("Training and validation accuracy")
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.legend()
+
+plt.show()
+
